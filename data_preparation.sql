c2ec43a5b4cd923699ea0146a66a2eb526ead275
jonas-butkevicius <jonas.butkevicius@vvk.lt>
2025-09-15 18:41:32 +0300

    Update data_preparation.sql

--- data_preparation.sql
+++ data_preparation.sql
@@ -5,20 +5,10 @@ SELECT
 FROM `peppy-hexagon-471408-c7.Yelp.yelp_business`,
   UNNEST(SPLIT(categories, ',')) AS category
 
---
-  
-  This creates a new table with the corrected 'elite' column
-CREATE OR REPLACE TABLE `peppy-hexagon-471408-c7.Yelp.yelp_user_cleaned` AS (
-  SELECT
-    -- This selects all columns EXCEPT the old, broken 'elite' column
-    * EXCEPT (elite),
-    CASE
-    -- If the original elite value is NULL or an empty string, it sets elite_years to an empty array ([]
-      WHEN elite IS NULL OR CAST(elite AS STRING) = '' THEN [] 
-      ELSE
-    -- This adds our new, fixed array column and names it 'elite_years'
-    REGEXP_EXTRACT_ALL(CAST(elite AS STRING), r'\d{4}')
-    END AS elite_years
-  FROM
-    `peppy-hexagon-471408-c7.Yelp.yelp_user`
-);
+
+UPDATE
+  `peppy-hexagon-471408-c7.Yelp.users`
+SET
+  elite = REGEXP_REPLACE(elite, r'\b20,20\b', '2020')
+WHERE
+  REGEXP_CONTAINS(elite, r'\b20,20\b');
