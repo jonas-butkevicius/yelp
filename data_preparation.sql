d6bec2293bbf2c12817065fce205aeb0d655a6c8
jonas-butkevicius <jonas.butkevicius@vvk.lt>
2025-09-15 15:19:06 +0300

    Update data_preparation.sql

--- data_preparation.sql
+++ data_preparation.sql
@@ -4,3 +4,15 @@ SELECT
   TRIM(category) AS category
 FROM `peppy-hexagon-471408-c7.Yelp.yelp_business`,
   UNNEST(SPLIT(categories, ',')) AS category
+
+
+-- This creates a new table with the corrected 'elite' column
+CREATE OR REPLACE TABLE `peppy-hexagon-471408-c7.Yelp.yelp_user_cleaned` AS (
+  SELECT
+    -- This selects all columns EXCEPT the old, broken 'elite' column
+    * EXCEPT (elite),
+    -- This adds our new, fixed array column and names it 'elite_years'
+    REGEXP_EXTRACT_ALL(CAST(elite AS STRING), r'\d{4}') AS elite_years
+  FROM
+    `peppy-hexagon-471408-c7.Yelp.yelp_user`
+);
